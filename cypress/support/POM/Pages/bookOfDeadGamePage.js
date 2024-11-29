5a4e3dbd452999e3777ae6e05c51b1e92ead7308
Aditya Panwar <a.panwar0709@gmail.com>
2024-11-30 00:52:11 +0200

    Added better assertion for game loading and added npm scripts to run the tests and get reports

--- cypress/support/POM/Pages/bookOfDeadGamePage.js
+++ cypress/support/POM/Pages/bookOfDeadGamePage.js
@@ -1,6 +1,9 @@
 class BookOfDeadGamePage {
     constructor() {
         this.gameFrame = "[id='ax-game-iframe']"
+        this.gameFrameBody = "0.contentDocument.body"
+        this.gameFramProgressBar = "#pngProgressBar"
+        this.gameFramProgressBarRawValue = "data-rawvalue"
     }
 
     // Method to verify the game is loading
@@ -10,21 +13,27 @@ class BookOfDeadGamePage {
         cy.url().should("include", "slots/book-of-dead");
 
         //Checking for the iFrame to be visible and have specific values
-        cy.get(this.gameFrame).should("be.visible").and(($iframe) => {
-            // Validate iframe is not empty and contains game-specific content
-            const src = $iframe.attr('src');
-            expect(src).to.include('bookofdead');
-        });
+        cy.get(this.gameFrame).should("be.visible")
 
-        //Validating if the canvas is visible
+        //Validating if the progress bar exists and its value changes
         cy.get(this.gameFrame)
-            .its('0.contentDocument.body')
-            .should('not.be.empty')
+            .its(this.gameFrameBody)
+            .should("not.be.empty")
             .then(cy.wrap)
-            .find('canvas')
-            .should('be.visible');
+            .find(this.gameFramProgressBar)
+            .invoke("attr", this.gameFramProgressBarRawValue)
+            .should("exist")
+            //getting the initial value of the loading bar value
+            .then((initialValue) => {
 
-        //TODO: Need to search for better assertions for validating the loading of the game
+                cy.get(this.gameFrame)
+                    .its(this.gameFrameBody)
+                    .then(cy.wrap)
+                    .find(this.gameFramProgressBar)
+                    .should("have.attr", this.gameFramProgressBarRawValue)
+                    //asserting the loading bar value changes
+                    .and('satisfy', (num) => num > initialValue)
+            });
     }
 }
 
